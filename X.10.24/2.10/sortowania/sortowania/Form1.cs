b1bb73de8a845c36e5d4138776665aa2879ab1c4
miloszmichalak <miloszmichalak150@gmail.com>
2024-10-02 13:19:18 +0200

    Dodanie czesci zadanie z lekcji 2.09

---
+++ X.10.24/2.10/sortowania/sortowania/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortowania
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void losuj_Click(object sender, EventArgs e)
+        {
+            int ilosc = int.Parse(ileLiczb.Text);
+            int[] liczbyT = losujLiczby(ilosc);
+            liczby.Text = "Liczby: " + String.Join(" ", liczbyT);
+            losuj.Enabled = false;
+        }
+
+        public static int[] losujLiczby(int n)
+        {
+            Random random = new Random();
+            int[] liczby = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                liczby[i] = random.Next(1, 100);
+            }
+            return liczby;
+        }
+
+        private void sort_Click(object sender, EventArgs e)
+        {
+            losuj.Enabled = true;
+
+        }
+    }
+}
---
+++ X.10.24/2.10/sortowania/sortowania/Sortowanie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortowania
+{
+    internal class Sortowanie
+    {
+        public static int[] BubbleSort(int[] arr)
+        {
+
+        }
+
+        public static int[] HoareSort(int[] nums)
+        {
+
+        }
+
+        public static int[] LomutoSort(int[] nums)
+        {
+
+        }
+    }
+}
