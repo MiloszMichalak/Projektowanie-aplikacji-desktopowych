0b72f7b72dc8af17f021ea3ddb0daf2b14476dbe
miloszmichalak <miloszmichalak150@gmail.com>
2024-10-09 13:13:57 +0200

    Dodanie zadanie z lekcji 9.10

---
+++ X.10.24/9.10/TODOZadanieEgzaminowe/TODOZadanieEgzaminowe/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TODOZadanieEgzaminowe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number1 = new System.Windows.Forms.TextBox();
+            this.number2 = new System.Windows.Forms.TextBox();
+            this.Check = new System.Windows.Forms.Button();
+            this.taknie = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(51, 25);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(100, 22);
+            this.number1.TabIndex = 0;
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(177, 25);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(100, 22);
+            this.number2.TabIndex = 1;
+            // 
+            // Check
+            // 
+            this.Check.Location = new System.Drawing.Point(301, 25);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(75, 23);
+            this.Check.TabIndex = 2;
+            this.Check.Text = "Sprawdz";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // taknie
+            // 
+            this.taknie.AutoSize = true;
+            this.taknie.Location = new System.Drawing.Point(51, 63);
+            this.taknie.Name = "taknie";
+            this.taknie.Size = new System.Drawing.Size(0, 16);
+            this.taknie.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.taknie);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox number1;
+        private System.Windows.Forms.TextBox number2;
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.Label taknie;
+    }
+}
+
---
+++ X.10.24/9.10/TODOZadanieEgzaminowe/TODOZadanieEgzaminowe/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TODOZadanieEgzaminowe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ErrorProvider error = new ErrorProvider();
+        private void Check_Click(object sender, EventArgs e)
+        {
+            taknie.Text = "";
+            error.SetError(number1, String.Empty);
+            error.SetError(number2, String.Empty);
+
+            int a;
+            int b;
+
+            bool correctNumber1 = int.TryParse(number1.Text, out a);
+            bool correctNumber2 = int.TryParse(number2.Text, out b);
+
+            if (!correctNumber1)
+            {
+                error.SetError(number1, "Podaj inta");
+            }
+            if (!correctNumber2)
+            {
+                error.SetError(number2, "Podaj inta");
+            }
+
+            if (correctNumber1 && correctNumber2)
+            {
+                if (czyZaprzyjaznione(a, b))
+                {
+                    taknie.Text = "Tak";
+                }
+                else
+                {
+                    taknie.Text = "Nie";
+                }
+            }
+        }
+
+        private static bool czyZaprzyjaznione(int a, int b)
+        {
+            if (sumaDzielnikow(a) == b && sumaDzielnikow(b) == a && a != b)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int sumaDzielnikow(int a)
+        {
+            int suma = 0;
+            for (int i = 1; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    suma += i;
+                }
+            }
+
+            return suma;
+        }
+    }
+}
