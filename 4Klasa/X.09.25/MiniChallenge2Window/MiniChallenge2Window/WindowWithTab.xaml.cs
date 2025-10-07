30b444bd486e12e8546ae8ae7114040782f38a3b
miloszmichalak <miloszmichalak150@gmail.com>
2025-10-07 09:29:46 +0200

    Dodano ui z zadania

---
+++ 4Klasa/X.09.25/MiniChallenge2Window/MiniChallenge2Window/WindowWithTab.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace MiniChallenge2Window;
+
+public partial class WindowWithTab : Window
+{
+    public WindowWithTab()
+    {
+        InitializeComponent();
+    }
+}
---
+++ 4Klasa/X.10.25/ZadanieWPF/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZadanieWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var regex = new Regex("^/d{2}[-]/d{3}$");
+        
+
+    }
+}
